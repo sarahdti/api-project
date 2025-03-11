a16087d16afe31fe7d7a752d9d7ace11b6e29b9d
sarahdti <sara.hedyati@gmail.com>
2025-03-11 17:13:10 +0330

    add packages and json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { FaArrowDownAZ } from "react-icons/fa6";
+
 function App() {
     return (
+        <>
         <div>next1code.ir</div>
+        <FaArrowDownAZ />
+        </>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './main.css'
+import 'bootstrap/dist/css/bootstrap.rtl.min.css'
+import { ThemeProvider } from "react-bootstrap";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+    <ThemeProvider
+     dir="rtl">
+  <App />
+</ThemeProvider>
 )
