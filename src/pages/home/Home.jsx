491e5bff8aef87a115aa6c00ea46acece12ef6ff
sarahdti <sara.hedyati@gmail.com>
2025-04-08 21:17:16 +0330

    add article page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
         <Container>
             <h1 style={{marginTop : '20px'}}>لیست مقالات</h1>
             <Row className="row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gy-4">
-                {articles.map(article=>(<Col><ArticleItem {...article}/></Col>))}
+                {articles.map(article=>(<Col key={article.id}><ArticleItem {...article}/></Col>))}
             </Row>
             
         </Container>
